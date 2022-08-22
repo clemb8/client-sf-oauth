04de7c113ba5ad726201d449b552f116f6804f5b
clemb8 <clem.boschet@gmail.com>
2022-08-22 20:20:15 +0200

    Add examples

---
+++ examples/exampleJWT.ts
@@ -0,0 +1,20 @@
+import { SF_JWTConnect, JWTParameters } from 'client-sf-oauth';
+
+async function JWTConnect() {
+
+  const JWTParameters: JWTParameters = {
+    secret: './key.pem',
+    clientId: process.env.clientId,
+    username: process.env.username
+  };
+  try {
+    const connection = new SF_JWTConnect(JWTParameters);
+    //console.log(connection);
+    const result = await connection.createJWTAndGetAccessToken('test');
+    console.log(result);
+  } catch (ex: any) {
+    console.log(ex);
+  }
+}
+
+JWTConnect();
---
+++ examples/examplePass.ts
@@ -0,0 +1,23 @@
+import { SF_PassConnect, PassParameters } from 'client-sf-oauth';
+
+async function PassConnect() {
+
+  const PassParameters: PassParameters = {
+    clientId: process.env.clientId,
+    secret: process.env.secret,
+    username: process.env.username,
+    password: process.env.password,
+    usertoken: process.env.usertoken,
+    host: process.env.host
+  };
+  try {
+    const connection = new SF_PassConnect(PassParameters);
+    //console.log(connection);
+    const result = await connection.requestAccessToken();
+    console.log(result);
+  } catch (ex: any) {
+    console.log(ex);
+  }
+}
+
+PassConnect();
