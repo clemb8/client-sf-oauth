51ff2dae00342495399cc7299a5ec271be0c2f3b
clemb8 <clem.boschet@gmail.com>
2022-08-23 17:26:54 +0200

    examples

--- examples/WebApp/index.ts
+++ examples/WebApp/index.ts
@@ -6,28 +6,20 @@ const app = express();
 const port = 3000;
 
 const parameters: WebAppParameters = {
-  clientId: '3MVG9t0sl2P.pByq7.k8JmYX8F7yd157cBqUnNqsfYyfPsYX.t8S9vnjyD3IKeZL9B3.LuGdihY6VFR3vwXoM',
-  clientSecret: '562EBD9D6A1093884D0CBA5619BC42A0BFC62D77C6B5F1B099803F2E750CED4E',
-  redirectURI: 'http://localhost:3000/getAccessToken',
-  host: 'https://wise-wolf-3lkdzp-dev-ed.my.salesforce.com/'
+  clientId: process.env.clientId,
+  clientSecret: process.env.clientSecret,
+  redirectURI: process.env.redirectUri,
+  host: process.env.host
 } 
 
 const connect = new SF_WebAppConnect(parameters);
 
 app.get('/', (req, res) => {
   res.redirect('/oauth');
-  // res.sendFile(path.resolve(__dirname, "./", "index.html"), (err: any) => {
-  //   if (err) {
-  //     res.status(500).send({
-  //       error: { status: 500, message: "Internal Server Error" },
-  //     });
-  //   }
-  // });
 })
 
 app.get('/oauth', async (req, res) =>  {
   const response = await connect.requestAuthCode();
-  console.log(response);
   res.redirect(response);
 });
 
